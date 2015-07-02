08155739f60c9015b72e859256041a50c1ec2192
ashic <ashic@live.com>
2015-07-02 03:33:43 +0100

    first query done

--- src/BasicQuerying/BasicQuerying/Program.cs
+++ src/BasicQuerying/BasicQuerying/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cassandra;
+using Cassandra.Data;
+using Cassandra.Data.Linq;
 
 namespace BasicQuerying
 {
@@ -10,6 +13,22 @@ namespace BasicQuerying
     {
         static void Main(string[] args)
         {
+            var cluster = Cluster.Builder()
+                .AddContactPoints("192.168.40.3", "192.168.40.4")
+                .WithCredentials("cassandra", "cassandra")
+                .Build();
+
+            var session = cluster.Connect("inventory");
+
+            var results = session.Execute("select * from products");
+            foreach (var result in results)
+            {
+                Console.WriteLine(result.GetValue<string>("sku"));
+            }
+            
+            
+            session.Dispose();
+            cluster.Dispose();
         }
     }
 }
